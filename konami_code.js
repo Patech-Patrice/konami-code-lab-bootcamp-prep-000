1fcd9720a1ab76df2b5c4630c3c216d1132b3fa1
Patrice Drayton <draytonpatrice@marilynssecret.org>
2018-09-18 15:36:18 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,4 +1,4 @@
-const codes =  
+const codes =  [
   "ArrowUp",
   "ArrowUp",
   "ArrowDown",
@@ -9,28 +9,29 @@ const codes =
   "ArrowRight",
   "b",
   "a"
-;
+];
 
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
-
-function init() {
-  //
-  var index = 0;
-  document.body.addEventListener('keydown', onKeyDownHandler);
-
-  function onKeyDownHandler(e) {
-    const key = parseInt(e.detail || e.which);
-    console.log('key', key);
-    if (key === code[index]) {
-      index++;
-
-      if (index === code.length) {
-        alert("YOU DID IT!");
-        index = 0;
-      }
-    } else {
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+var keyPressed = [];
+var index = 0;
+ function init() {
+  // Write your JavaScript code inside the init() function
+  const body = document.body;
+ } 
+  body.addEventListener("keydown", konamiFunc);
+}
+ function konamiFunc(event) {
+  let input = parseInt(event.which);
+  console.log(input);
+  keyPressed.push(input);
+   if(keyPressed[index] === code[index]) {
+    index++;
+     if(index === code.length) {
+      alert("Yay!");
       index = 0;
     }
+   } else {
+    index = 0;
+    keyPressed = [];
   }
 }
