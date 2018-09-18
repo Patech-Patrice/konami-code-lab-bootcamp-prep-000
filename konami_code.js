be7d4393e0142891419076b89ad1263e2f3ddfb0
Patrice Drayton <draytonpatrice@marilynssecret.org>
2018-09-18 15:38:05 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -11,27 +11,21 @@ const codes =  [
   "a"
 ];
 
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
-var keyPressed = [];
-var index = 0;
- function init() {
-  // Write your JavaScript code inside the init() function
-  const body = document.body;
- } 
-  body.addEventListener("keydown", konamiFunc);
-}
- function konamiFunc(event) {
-  let input = parseInt(event.which);
-  console.log(input);
-  keyPressed.push(input);
-   if(keyPressed[index] === code[index]) {
-    index++;
-     if(index === code.length) {
-      alert("Yay!");
-      index = 0;
-    }
-   } else {
-    index = 0;
-    keyPressed = [];
-  }
+function init() {
+  let index = -1;
+  document.body.addEventListener('keydown', (event) => {
+     index++;
+     console.log(index, event);
+     if (codes[index] === event.key) {
+        console.log("sequence OK");
+        if (index + 1 === codes.length) {
+          alert("Congratulations!");
+          console.log("YAY!!!");
+          index = -1;
+        }
+     } else {
+        console.log("sequence BAD");
+        index = -1;
+     }
+   });
 }
